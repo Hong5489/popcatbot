1519aba38fbda4a6986ecf701e550917c3338036
Hong5489 <hongwei5489@gmail.com>
2021-08-14 17:40:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# PopCat Bot
+```
+Popcat for lazy people
+```
+
+## Requirements
+- Python 2 / 3
+- pip 
+
+## Instal
+```
+pip install requests
+```
+## How to Use
+1. Goto https://popcat.click/
+2. Right Click > Inspect
+3. Open Network Tab
+4. Popcat few times
+5. Network tab will appear a request URL (https://stats.popcat.click/pop)
+6. Scroll down and copy the `captcha_token` and `token` value like the image below:
+
+![image](image1.png)  
+
+7. Replace `captcha_token` and `token` in the `popcat.py` script
+8. Run the script by command `python popcat.py` or `python3 popcat.py`
+9. Enjoy =)
---
+++ popcat.py
@@ -0,0 +1,41 @@
+import requests
+import time
+
+# Edit here
+captcha_token = ""
+token = ""
+# Note pop count maximum is 800
+pop_count = 800
+
+total = 0
+headers = {
+    'Connection': 'keep-alive',
+    'Content-Length': '0',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+    'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+    'Accept': 'application/json, text/plain, */*',
+    'sec-ch-ua-mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+    'Origin': 'https://popcat.click',
+    'Sec-Fetch-Site': 'same-site',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://popcat.click/',
+    'Accept-Language': 'en-GB,en;q=0.9',
+}
+
+params = (
+    ('pop_count', str(pop_count)),
+    ('captcha_token', captcha_token),
+    ('token', token),
+)
+
+
+while(1):
+    response = requests.post('https://stats.popcat.click/pop', headers=headers, params=params)
+    if response.status_code == 201:
+        total += pop_count
+
+    print("Total poped: "+str(total))
+    time.sleep(30)
